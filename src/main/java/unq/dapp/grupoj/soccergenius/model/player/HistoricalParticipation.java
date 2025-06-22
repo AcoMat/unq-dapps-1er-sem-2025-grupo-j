9d84c0891a3ccb2312fe0978aae105f5493da36f
Fernando Muñoz <fmunoz@exisoft.com.ar>
2025-06-22 19:20:58 -0300

    fix sonarQube issue unused private fields HistoricalParticipation

--- src/main/java/unq/dapp/grupoj/soccergenius/model/player/HistoricalParticipation.java
+++ src/main/java/unq/dapp/grupoj/soccergenius/model/player/HistoricalParticipation.java
@@ -11,16 +11,4 @@ public class HistoricalParticipation {
     @ManyToOne
     @JoinColumn(name = "player_id")
     private HistoricalParticipationsSummary playerHistory;
-
-    private String season;
-    private String championshipName;
-
-    //private int totalGamesPlayed;
-    //private int totalMinsPLayed;
-    //private int totalGoals;
-    //private int totalAssists;
-    //private int totalYellowCards;
-    //private int totalRedCards;
-
-    private int rating;
 }
